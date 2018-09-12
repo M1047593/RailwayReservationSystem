a9713ac95bcc055171b2ef2aad46615a844ecb73
Kannan Kalyanaraman <M1047593@mindtree.com>
2018-09-12 19:39:08 +0530

    commit

--- RailwayReservation.Services/App_Start/WebApiConfig.cs
+++ RailwayReservation.Services/App_Start/WebApiConfig.cs
@@ -13,10 +13,11 @@ namespace RailwayReservation.Services
 
             // Web API routes
             config.MapHttpAttributeRoutes();
+            config.EnableCors();
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "api/{controller}/{action}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
---
+++ RailwayReservation.Services/Controllers/ReservationController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using RS.Repository.Admin;
+using System.Web.Http;
+using System.Web.Http.Cors;
+using RS.Data;
+
+namespace RailwayReservation.Services.Controllers
+{
+    [EnableCors(origins: "*", headers: "*", methods: "*")]
+    public class ReservationController : ApiController
+    {  
+    
+        RS.Repository.Admin.RailwayRespo r = new RS.Repository.Admin.RailwayRespo();
+      
+
+
+        [HttpGet]
+        public string invoke()
+        {
+            return "success";
+        }
+
+
+        [HttpPost]
+        public bool ValidateLogin([FromBody]RS.Data.Entity.Login Login)
+        {
+            return r.ValidateUser(Login.UserName, Login.Password);
+        }
+
+    }
+}
